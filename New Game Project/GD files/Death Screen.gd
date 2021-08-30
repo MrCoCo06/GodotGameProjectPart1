7be8ec8f2334e4574a42a1451e943b3e68d93690
Jonathan A. Velazquez <jonathanarmando175@yahoo.com>
2021-08-29 22:31:52 -0400

    Revert "Revert "Revert "Major Update"""
    This reverts commit 0db26bb5637370f340e9fa1922cf5c36dfacc8c0.

--- New Game Project/GD files/Death Screen.gd
+++ New Game Project/GD files/Death Screen.gd
@@ -1,16 +1,17 @@
 extends Control
 
-
+#When called, makes Death Menu visible and pause every background actions(you can't move player)
 func death():
 	
 	visible = not visible
-
-
-
-func _on_revive_pressed():
+	get_tree().paused = not get_tree().paused
+#When called, restart the scene
+func _on_Button_pressed():
 	visible = not visible
 	get_tree().reload_current_scene()
+	get_tree().paused = not get_tree().paused
 
-
-func _on_exitToMenu_pressed():
+#when called, player is sent back to main menu
+func _on_Button2_pressed():
 	get_tree().change_scene("res://Scenes/menu.tscn")
+	get_tree().paused = not get_tree().paused
--- New Game Project/GD files/menuScript.gd
+++ New Game Project/GD files/menuScript.gd
@@ -2,7 +2,7 @@ extends TextureButton
 
 #Start mission
 func _on_start_pressed():
-	get_tree().change_scene("res://Scenes/mission1.tscn")
+	get_tree().change_scene("res://Scenes/MissionSelect.tscn")
 
 #Close game
 func _on_close_pressed():
