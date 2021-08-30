136dd7973158d063fc08d16ae8fc993440fa1776
Jonathan A. Velazquez <jonathanarmando175@yahoo.com>
2021-08-29 22:28:16 -0400

    Major Update
    Added basic enemy combat, expanded level design, added finishing flag, comments where added to the code. The intention of this update is to have a playable demo with only one level. It is enough to show how the games run. More update will be done in the future, but as for today, AUG-29-2021, and forward, I'll be making minor changes or adding new feature slowly to solely focus on more advanced projects that I have on mind.

--- New Game Project/GD files/MissionSelect.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-# if button is clicked on, it will load the first mission
-func _on_firstMission_pressed():
-	get_tree().change_scene("res://Scenes/mission1.tscn")
-	
-#if the button is clicked, it will move back to the first main menu screen
-func _on_goBack_pressed():
-	get_tree().change_scene("res://Scenes/menu.tscn")
-
--- New Game Project/GD files/Player.gd
+++ New Game Project/GD files/Player.gd
@@ -4,7 +4,6 @@ extends KinematicBody2D
 var score : int = 0
 var health : int = 100
 
-
 var speed : int = 200
 var jumpForce : int = 1000
 var gravity : int = 2000
@@ -12,9 +11,8 @@ var gravity : int = 2000
 var jumpcount : int = 0
 var vel : Vector2 = Vector2()
 
-
-onready var deathScreen : Node = get_node("CanvasLayer/DeathScreen")
-onready var anim : AnimatedSprite = get_node("ASprite")
+onready var deathScreen : Node = get_node("CanvasLayer/Death Screen")
+onready var anim : Sprite = get_node("ASprite")
 onready var ui : Node = get_node("CanvasLayer/UI")
 onready var audioPlayer : Node = get_node("/root/Mission #1/Camera2D/AudioPlayer")
 
@@ -30,7 +28,8 @@ func _process(delta):
 			vel.x -= speed /2
 		if is_on_floor() == true:
 			anim.play("Walking")
-		
+		if Input.is_action_pressed("move_left") == false and Input.is_action_pressed("move_right") == false and Input.is_action_pressed("jump") == false and is_on_floor():
+			anim.play("Standing")
 	
 	#move right
 	if Input.is_action_pressed("move_right"):
@@ -39,7 +38,8 @@ func _process(delta):
 			vel.x += speed /2
 		if is_on_floor() == true:
 			anim.play("Walking")
-		
+		if Input.is_action_pressed("move_left") == false and Input.is_action_pressed("move_right") == false and Input.is_action_pressed("jump") == false and is_on_floor():
+			anim.play("Standing")
 	
 	#jump input
 	if is_on_floor():
@@ -54,25 +54,28 @@ func _process(delta):
 	
 	if(jumpcount<3) and is_on_floor() == false:
 		if Input.is_action_just_pressed("jump"):
-			vel.y -= vel.y + jumpForce / 2
+			vel.y -= jumpForce / 2
 		
 	
-	standing_anim()
+	if Input.is_action_pressed("move_left") == false and Input.is_action_pressed("move_right") == false and Input.is_action_pressed("jump") == false and is_on_floor():
+		anim.play("Standing")
+	
+	
 	
 	#applying the velocity
 	vel = move_and_slide(vel, Vector2.UP)
-
+	
 	#gravity
 	vel.y += gravity * delta
+	
 
+	
 	#sprite direction
 	if vel.x < 0:
 		anim.flip_h = true
 	elif vel.x > 0:
 		anim.flip_h = false
-	#if player falls into void, it will return to the beginning
-	if position.y >= 1000:
-		position = Vector2(50, 450)
+	
 	
 func takedamage(a):
 	health -= a
@@ -84,13 +87,12 @@ func takedamage(a):
 	
 
 func collect_coin(value):
+	
 	score += value
 	ui.set_score_text(score)
 	audioPlayer.play_coin_sfx()
+	
 
-func standing_anim():
-	if Input.is_action_pressed("move_left") == false and Input.is_action_pressed("move_right") == false and Input.is_action_pressed("jump") == false and is_on_floor():
-		anim.play("Standing")
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://Scenes/menu.tscn")
